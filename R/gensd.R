04b4d3512c3ee0f0cd8fa4c5a608b022f9065de7
Sewanou <honfosewanou@gmail.com>
2019-08-24 13:43:02 +0100

    Package initialization

---
+++ R/gensd.R
@@ -0,0 +1,29 @@
+#' @name gensd
+#' @title Attributes skewed and dependent
+#' @description Generates spatial attributes that are skewed and dependent.
+#'
+#'
+#' @usage     gensd (size, sk, dep, ...)
+#'
+#'
+#' @param size a vector of two elements specifying respectively the number of rows and columns of the matrix of attributes to be generated.
+#' @param sk a scalar that represents the skewness coefficient to be considered.
+#' @param dep a scalar that represents the dependence correlation between values within a given attribute.
+#' 
+#' @return Returns a list:
+#' \item{spadata}{a matrix containing expected data.}
+#' \item{init}{a recall of initial parameters}
+#'
+#'
+#'
+#' @author Sewanou Honfo \url{<honfosewanou@gmail.com>}  and Brezesky Kotanmi \url{<kotangaebrezesky@gmail.com>} and Eunice Gnanvi \url{<eunysgnanvi@gmail.com>} / LABEF_08_2019
+#'
+#'
+#' @examples
+#'
+
+gensd <- function (size, sk, dep, ...)
+{
+  
+  
+}
